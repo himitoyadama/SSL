4dbc76ea059183eab4737b920a5658d7a4b7239d
himitoyadama <119666780+himitoyadama@users.noreply.github.com>
2023-11-02 10:30:18 +0900

    Create MainPage.xaml.cs

---
+++ MainPage.xaml.cs
@@ -0,0 +1,22 @@
+namespace Test_U
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync("Hello, World!");
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
+    }
+}
